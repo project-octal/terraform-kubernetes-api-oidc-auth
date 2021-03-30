4031e52eb196d42fc9488f988c6839d1df07d926
Dylan Turnbull <dylanturn@gmail.com>
2021-03-30 09:21:19 -0500

    added lock file

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ resource "kubernetes_cluster_role_binding" "oidc_cluster_role_bindings" {
   for_each = var.cluster_role_bindings
 
   metadata {
-    name = "oidc-${each.value["oidc_group_name"]}-${each.value["cluster_role_name"]}"
+    name   = "oidc-${each.value["oidc_group_name"]}-${each.value["cluster_role_name"]}"
     labels = {}
   }
   role_ref {
     api_group = "rbac.authorization.k8s.io"
-    kind = "ClusterRole"
-    name = each.value["cluster_role_name"]
+    kind      = "ClusterRole"
+    name      = each.value["cluster_role_name"]
   }
   subject {
     kind = "Group"
--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "oidc_groups_prefix" {
-  type = string
+  type        = string
   description = "The prefix attached to OIDC groups bound to cluster roles. Needs to match the value given to the kube-api-server argument `--oidc-groups-prefix`"
-  default = "oidc:"
+  default     = "oidc:"
 }
 
 variable "cluster_role_bindings" {
   type = set(object({
     cluster_role_name = string
-    oidc_group_name = string
+    oidc_group_name   = string
   }))
   description = "The name of the OIDC groups to bind to the builtin `cluster-admin` cluster role."
-  default = null
+  default     = []
 }
