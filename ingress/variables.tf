958f9bba3dac9157691fb32d23d27da0a520b811
Dylan Turnbull <dylanturn@gmail.com>
2021-03-30 09:21:34 -0500

    terraform fmt

--- ingress/variables.tf
+++ ingress/variables.tf
@@ -1,43 +1,43 @@
 variable "ingress_class" {
-  type = string
+  type        = string
   description = "Specifies which ingress class should be used. Defaults to the default cluster class"
-  default = null
+  default     = null
 }
 variable "ratelimit_average" {
-  type = number
+  type        = number
   description = "`ratelimit_average` is the maximum rate, by default in requests per second, allowed from a given source. Defaults to `0`"
-  default = 0
+  default     = 0
 }
 variable "ratelimit_period" {
-  type = number
+  type        = number
   description = "`ratelimit_period`, in combination with `ratelimit_average`, defines the actual maximum rate. Defaults to `1` second"
-  default = 1
+  default     = 1
 }
 variable "ratelimit_burst" {
-  type = number
+  type        = number
   description = "`ratelimit_burst` is the maximum number of requests allowed to go through in the same arbitrarily small period of time. Defaults to `1`"
-  default = 1
+  default     = 1
 }
 
 variable "apiserver_url" {
-  type = string
+  type        = string
   description = ""
 }
 variable "apiserver_service_name" {
-  type = string
+  type        = string
   description = ""
-  default = "kubernetes"
+  default     = "kubernetes"
 }
 variable "apiserver_service_namespace" {
-  type = string
+  type        = string
   description = ""
-  default = "default"
+  default     = "default"
 }
 variable "cluster_cert_issuer" {
-  type = string
+  type        = string
   description = ""
 }
 variable "labels" {
-  type = map(string)
+  type        = map(string)
   description = ""
 }
